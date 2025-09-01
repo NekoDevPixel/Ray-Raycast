41bcd7146f6008d53f01bf72e922ff747ed6d5e4
LEE_SANG_JUN <sahwang04@gmail.com>
2025-09-01 22:55:05 +0900

    RaycastHit2D 연습

--- Assets/RayPoint.cs
+++ Assets/RayPoint.cs
@@ -4,12 +4,15 @@ public class RayPoint : MonoBehaviour
 {
     void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = Vector2.zero;
 
-        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        // Ray ray = Camera.main.ScreenPointToRay(mousePos);
 
 
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, direction);
+
+        if (hit.collider != null)
         {
             Debug.Log("Hit object: " + hit.collider.name);
         }
