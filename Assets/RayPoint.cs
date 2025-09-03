118b26ea7aa199a49216be29ccf5ce638b45f9e1
NekoDevPixel <sahwang04@gmail.com>
2025-09-03 11:05:44 +0900

    Physics2D.OverlapPoint를 사용하여 마우스로 물체 위치 옮기는 로직

--- Assets/RayPoint.cs
+++ Assets/RayPoint.cs
@@ -2,19 +2,76 @@ using UnityEngine;
 
 public class RayPoint : MonoBehaviour
 {
+    private Collider2D hit;
+    private Vector2 mousePos;
+    private GameObject selectobject;
+    private bool mvobj = false;
+
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = Vector2.zero;
+        MouseClick();
+        
+    }
+
+    void FixedUpdate()
+    {
+        MoveObject();
+    }
+
+    void MouseClick()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Vector2 direction = Vector2.zero;
 
         // Ray ray = Camera.main.ScreenPointToRay(mousePos);
 
 
-        RaycastHit2D hit = Physics2D.Raycast(mousePos, direction);
+        // RaycastHit2D hit = Physics2D.Raycast(mousePos, direction);
 
-        if (hit.collider != null)
+        // if (hit.collider != null)
+        // {
+        //     Debug.Log("Hit object: " + hit.collider.name);
+        // }
+
+        // hit = Physics2D.OverlapPoint(mousePos);
+
+        // if (hit != null)
+        // {
+        //     // Debug.Log("Hit object: " + hit.name);
+        //     if (Input.GetMouseButtonDown(0) && mvobj == false)
+        //     {
+        //         Debug.Log("Clicked on object: " + hit.name);
+        //         mvobj = true;
+        //     }
+        //     else if (Input.GetMouseButtonDown(0) && mvobj == true)
+        //     {
+        //         mvobj = false;
+        //     }
+        // }
+
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Hit object: " + hit.collider.name);
+            hit = Physics2D.OverlapPoint(mousePos);
+
+            if (hit != null)
+            {
+                selectobject = hit.gameObject;// hit.gameobject 참조
+                mvobj = !mvobj;
+            }
+            else
+            {
+                selectobject = null;
+                mvobj = false;
+            }
+        }
+    }
+    void MoveObject()
+    {
+        if (mvobj && selectobject != null)
+        {
+            selectobject.transform.position = mousePos;
+            //hit.gameobject.transform.position의 값도 변경된다
+
         }
     }
 }
